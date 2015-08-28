f0810a127f41b353141db4a6226877ee6401fdc7
kewenxing <kewenxing@gmail.com>
2015-08-28 23:06:08 +0800

    weixin interface

---
+++ menu.js
@@ -0,0 +1,52 @@
+{
+  "button": [{
+    "name": "会员天地",
+    "sub_button": [{
+      "type": "click",
+      "name": "入会",
+      "key": "member_join"
+    }, {
+      "type": "click",
+      "name": "绑定房产",
+      "key": "member_binding_house"
+    }, {
+      "type": "click",
+      "name": "会员信息",
+      "key": "member_info"
+    }]
+  }, {
+    "name": "我的小区",
+    "sub_button": [{
+      "type": "click",
+      "name": "小区微官网",
+      "key": "uptown_website"
+    }, {
+      "type": "click",
+      "name": "物业报修",
+      "key": "uptown_service"
+    }, {
+      "type": "click",
+      "name": "小区公告",
+      "key": "uptown_advice"
+    }, {
+      "type": "click",
+      "name": "居委会公告",
+      "key": "uptown_jwh_advice"
+    }, {
+      "type": "click",
+      "name": "会员卡",
+      "key": "uptown_membercard"
+    }]
+  }, {
+    "name": "乐翻天",
+    "sub_button": [{
+      "type": "click",
+      "name": "免费礼品",
+      "key": "shopping_free"
+    }, {
+      "type": "click",
+      "name": "乐翻天",
+      "key": "shopping_website"
+    }]
+  }]
+}
